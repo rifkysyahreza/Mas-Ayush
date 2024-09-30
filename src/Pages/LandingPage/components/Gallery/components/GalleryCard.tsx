9e91b1b7945d3c2cb5b0a5b0b82eb587cfb726f7
Rifky Syahreza <rsyahrezafs@gmail.com>
2024-09-30 10:05:28 +0700

    feat: commit

--- src/Pages/LandingPage/components/Gallery/components/GalleryCard.tsx
+++ src/Pages/LandingPage/components/Gallery/components/GalleryCard.tsx
@@ -12,12 +12,24 @@ const GalleryCardToLeft: FC<GalleryCardProps> = ({ name, link, image }) => {
       <div className=" px-12 py-14 shrink-0 bg-[#C7D0D9] animate-infinite-scroll group-hover:paused hover:drop-shadow-xl">
         <a href={link}>
           <img src={image} alt={name} className=" w-80 h-48 object-cover" />
+          <a
+            className="invisible group-hover:visible font-medium absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white rounded-md px-4 py-3 border border-black"
+            href={link}
+          >
+            Visit the website
+          </a>
         </a>
       </div>
 
       <div className="px-12 py-14 bg-[#C7D0D9] shrink-0  animate-infinite-scroll-reverse group-hover:paused hover:drop-shadow-xl">
         <a href={link}>
           <img src={image} alt={name} className=" w-80 h-48 object-cover" />
+          <a
+            className="invisible group-hover:visible font-medium absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white rounded-md px-4 py-3 border border-black"
+            href={link}
+          >
+            Visit the website
+          </a>
         </a>
       </div>
     </div>
